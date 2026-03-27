d1dd042c007d6c4681c0846ada15f6017c378692
PaulaTambunan <paulatambunan10@gmail.com>
2026-03-27 17:18:20 +0700

    Driver1.java

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -8,7 +8,50 @@ public class Driver1 {
 
     public static void main(String[] _args) {
 
-        // codes
+      package fintech.driver;
+import java.util.ArrayList;
+import java.util.Scanner;
+import fintech.model.*;
+
+/**
+ * 12S24025 Paula Gevriella Tambunan
+ */
+public class Driver1 {
+    
+
+    public static void main(String[] _args) {
+     Scanner masukan = new Scanner(System.in);
+        ArrayList<Account> accounts = new ArrayList<>();
+        
+        while (masukan.hasNext()) {
+            
+            String input = masukan.nextLine();
+            
+          
+            
+           
+            String[] inputSegments = input.split("#");
+            String command = inputSegments[0];
+            String accountUsername = inputSegments[1];
+            String accountName = inputSegments[2];
+            
+            if (command.equals("create-account")) {
+                
+                Account account = new Account(accountUsername, accountName);
+                
+                
+                accounts.add(account);
+                System.out.println(account.getUsername() + "|" + account.getName() + "|" + account.getBalance());
+            }
+            
+            if (input.equals("---")) {
+                break;
+            }
+        }
+        masukan.close();
+    }
+}
+
 
     }
 
